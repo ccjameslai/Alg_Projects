a671483692a2e72aa1d80af637c71f87080b023d
James <spidyjames@gmail.com>
2018-03-16 00:42:47 +0800

    Add two functions called policy_evaluation and policy_improvement.

--- Reinforcement learning/value_func_iter.py
+++ Reinforcement learning/value_func_iter.py
@@ -5,12 +5,7 @@ Created on Mon Mar 12 21:50:44 2018
 @author: James
 """
 
-states = [s for s in range(0,16)]
-values = [0] * 16
-r = -1
-policy = 0.25
-
-for k in range(10):
+def policy_evaluation(values,r,policy):
     temp_values = [0] * 16
     for i in range(len(values)):
         if i == 0:
@@ -20,24 +15,67 @@ for k in range(10):
         if (i >= 1) and (i <= 2):
             temp_values[i] = policy * (r+values[i]) + policy * (r+values[i - 1]) \
             + policy * (r+values[i + 1]) + policy * (r+values[i + 4])
+            temp_values[i] = round(temp_values[i],1)
         elif i == 3:
             temp_values[i] = policy * (r+values[i - 1]) + policy * (r+values[i]) \
             + policy * (r+values[i]) + policy * (r+values[i + 4])
+            temp_values[i] = round(temp_values[i],1)
         elif  (i == 4) or (i == 8):
             temp_values[i] = policy * (r+values[i - 4]) + policy * (r+values[i]) \
             + policy * (r+values[i + 1]) + policy * (r+values[i + 4])
+            temp_values[i] = round(temp_values[i],1)
         elif (i == 5) or (i == 6) or (i == 9) or (i == 10):
             temp_values[i] = policy * (r+values[i - 1]) + policy * (r+values[i - 4]) \
             + policy * (r+values[i + 1]) + policy * (r+values[i + 4])
+            temp_values[i] = round(temp_values[i],1)
         elif (i == 7) or (i == 11):
             temp_values[i] = policy * (r+values[i - 1]) + policy * (r+values[i - 4]) \
             + policy * (r+values[i]) + policy * (r+values[i + 4])
+            temp_values[i] = round(temp_values[i],1)
         elif i == 12:
             temp_values[i] = policy * (r+values[i]) + policy * (r+values[i - 4]) \
             + policy * (r+values[i + 1]) + policy * (r+values[i])
+            temp_values[i] = round(temp_values[i],1)
         else:
             temp_values[i] = policy * (r+values[i - 1]) + policy * (r+values[i - 4]) \
             + policy * (r+values[i + 1]) + policy * (r+values[i])
-            
-    values = temp_values
-print(values)
+            temp_values[i] = round(temp_values[i],1)
+    return temp_values
+
+def policy_improvement(values,r):
+    pi = {'1':[],'2':[],'3':[],'4':[],'5':[],'6':[],'7':[],'8':[],'9':[],'10':[],'11':[],'12':[],'13':[],'14':[]}
+    actions = ['<','^','>', 'v']
+    for i in range(len(values)):
+        if i == 0 or i == 15:
+            continue
+        if (i >= 1) and (i <= 2):
+            temp = [(r+values[i - 1]), (r+values[i]) ,(r+values[i + 1]) ,(r+values[i + 4])]
+            pi[str(i)] = actions[temp.index(max(temp))]
+        elif i == 3:
+            temp = [(r+values[i - 1]), (r+values[i]) ,(r+values[i]) ,(r+values[i + 4])]
+            pi[str(i)] = actions[temp.index(max(temp))]
+        elif  (i == 4) or (i == 8):
+            temp = [(r+values[i]),(r+values[i - 4]),(r+values[i + 1]),(r+values[i + 4])]
+            pi[str(i)] = actions[temp.index(max(temp))]            
+        elif (i == 5) or (i == 6) or (i == 9) or (i == 10):
+            temp = [(r+values[i - 1]),(r+values[i - 4]) ,(r+values[i + 1]),(r+values[i + 4])]
+            pi[str(i)] = actions[temp.index(max(temp))]    
+        elif (i == 7) or (i == 11):
+            temp = [(r+values[i - 1]),(r+values[i - 4]) ,(r+values[i]),(r+values[i + 4])]
+            pi[str(i)] = actions[temp.index(max(temp))]
+        elif i == 12:
+            temp = [(r+values[i]),(r+values[i - 4]),(r+values[i + 1]),(r+values[i])]
+            pi[str(i)] = actions[temp.index(max(temp))]
+        else:
+            temp = [(r+values[i - 1]) ,(r+values[i - 4]) ,(r+values[i + 1]),(r+values[i])]
+            pi[str(i)] = actions[temp.index(max(temp))]
+    return pi        
+
+values = [0] * 16
+r = -1
+policy = 0.25
+
+for k in range(10):
+    values = policy_evaluation(values,r,policy)
+    pi = policy_improvement(values,r)
+print(pi)
